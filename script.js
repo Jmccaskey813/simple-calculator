e117e96f1342a13c990d2296811faaf0a6a818f3
Jacob McCaskey <mccaskey316@gmail.com>
2020-01-13 11:14:40 -0500

    adds input functionality

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const calculator = document.querySelector('.grid');
 const keys = document.querySelector('.keys');
 const display = document.querySelector('.display');
+let a;
+let aValue= false;
+let b;
+let bValue= false;
+let operator;
 
 keys.addEventListener('click', e=>{
+    let calculateNums = ()=>{
+        if (aValue === true && bValue === false){
+            b = displayedNum;
+            bValue= true;
+            let result = Math.floor(parseInt(a) + operator + parseInt(b))
+            displayedNum = result;
+            console.log(result);
+        }
+       }
+       
+
     if (e.target.matches('button')){
         const key = e.target;
         const action = key.dataset.action;
@@ -10,15 +26,22 @@ keys.addEventListener('click', e=>{
         var displayedNum = display.textContent 
         if (action === 'decimal') display.textContent = displayedNum + '.';
         if (!action ) {
-            (displayedNum === '0')? display.textContent = keyContent 
+            (displayedNum === '0' || displayedNum === '')? display.textContent = keyContent 
                                 : display.textContent = displayedNum + keyContent;
-            console.log(displayedNum) 
+            // console.log(displayedNum) 
         }
         if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide' ){
-            calculator.dataset.previousKeyType = 'operator'
-            console.log(calculator.dataset.previousKeyType);
+            operator = keyContent;
+             if (aValue === false){
+                 a= displayedNum;
+                 aValue= true;
+                displayedNum === '0';
+                display.textContent= '';
+             };
         }
+        if (action === 'calculate'){calculateNums()};
     }
     
     
 }, false);
+
